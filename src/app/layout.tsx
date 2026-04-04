8cdf669cd24a30cf79af3ab54de22b457e116061
Z User <z@container>
2026-04-04 13:57:18 +0000

    0a148765-7986-42d1-b082-3c127f1097a5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,8 +57,7 @@ export default function RootLayout({
         <link
           rel="stylesheet"
           href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-          integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/mi2yoY5qXAu05AwGEEQ=="
-          crossOrigin=""
+          crossOrigin="anonymous"
         />
       </head>
       <body
