28ad5487b784e1f2a95308d453d6c591f5c4cafe
Z User <z@container>
2026-04-04 10:38:07 +0000

    821c77d0-8054-4bc4-b8e5-4e9c5ee815be

--- start.sh
+++ start.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 cd /home/z/my-project
 while true; do
-  echo "=== Starting dev server at $(date) ===" >> /home/z/my-project/server.log
-  bun run dev >> /home/z/my-project/dev.log 2>&1
-  EXIT_CODE=$?
-  echo "=== Server exited with code $EXIT_CODE at $(date), restarting in 3s... ===" >> /home/z/my-project/server.log
+  echo "Starting dev server..."
+  npx next dev --turbopack -p 3000 2>&1 | tee -a /home/z/my-project/dev.log
+  echo "Server died, restarting in 3s..."
   sleep 3
-  rm -rf /home/z/my-project/.next
 done
